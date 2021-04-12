8245df4ecc48f820e3e764e356aa4721c48f1de5
Sheuh Xiong <sheuhxiong@gmail.com>
2021-04-11 22:59:53 -0400

    Added bg-img to all three cards

--- src/App.js
+++ src/App.js
@@ -13,11 +13,19 @@ function App() {
       <div className="header-name">
         <h1>Sheuh Xiong</h1>
       </div>
+      {/* <div className="section-header">
+        <h2>About</h2>
+      </div> */}
       <div className="about">
-        <p>I'm a front-end developer who builds stuff with HTML, CSS, React, and Firebase. Check out my three side-projects below!</p>
+        {/* <p>I'm a front-end developer who builds stuff with HTML, CSS, React JS, and Firebase. In my spare time I enjoy hiking, cooking, reading, playing Go/Baduk, and watching Space-related videos on YouTube. Check out my side-projects below!</p> */}
+        <p>I'm a front-end developer who builds stuff with HTML, CSS, React JS, and Firebase.</p>
+        <p>Check out my favourite side-projects below!</p>
       </div>
-      <div className="projects-header-container">
+      <div className="section-header projects-header-container">
         <h2>Projects</h2>
+      </div>
+      <div className="text-area">
+        
       </div>
       <div className="projects-container">
         <div className="card card-0"></div>
