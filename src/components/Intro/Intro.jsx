8f5713b90de6d52586e6fb27a49ab182566401b5
Sheuh Xiong <sheuhxiong@gmail.com>
2021-04-13 16:30:10 -0400

    Adjusted Intro component p tag

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -4,7 +4,7 @@ function Intro() {
   return (
     <div className="intro">
       <p>I'm an aspiring front-end developer who builds stuff with HTML, CSS, React JS, and Firebase.</p>
-      <p>Read more about me in the code below, or check out my favourite side projects!</p>
+      <p>Read more about me in the code below or check out my favourite side projects!</p>
     </div>
   );
 }
