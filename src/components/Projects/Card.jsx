737fb2443dfaef9ba4676cc06e8a603f63adc02a
Sheuh Xiong <sheuhxiong@gmail.com>
2021-04-13 13:09:00 -0400

    Improved styling for Card component

--- src/components/Projects/Card.jsx
+++ src/components/Projects/Card.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
-import { FaExternalLinkAlt } from 'react-icons/fa';
+import { FaExternalLinkAlt, FaGithub } from 'react-icons/fa';
 
-function Card({ className, num }) {
+function Card({ className, num, projectTitle, projectInfo }) {
 
   return (
     <div className="card-overflow-container">
       <div className={className}>
         <div className={`dark-bg-${num}`}>
+          <div className="card-project-info">
+            <h1>{projectTitle}</h1>
+            <span>{projectInfo}</span>
+          </div>
           <div className="card-button-container">
-            <button className="view-code-button">View Code</button>
-            <button className="live-demo-button">Live Demo <FaExternalLinkAlt className="fa-external-link" /></button>
+            <button className="view-code-button">View Code <FaGithub className="fa-icon" /></button>
+            <button className="live-demo-button">Live Demo <FaExternalLinkAlt className="fa-icon" /></button>
           </div>
         </div>
       </div>
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -3,11 +3,33 @@ import Card from './Card';
 import "./Projects.css";
 
 function Projects() {
+
+  const details = {
+    findLaCloche: "An online database for users to share images and reviews of campsites on the La Cloche Silhouette Trail.",
+    finDIO: "A mini-game influenced by Where's Waldo: find the character DIO of JoJo's Bizarre Adventure.",
+    toDo: "A task management web-application inspired by Microsoft To Do."
+  };
+
   return (
     <div className="projects-container">
-      <Card className="card card-0-bg" num={0} />
-      <Card className="card card-1-bg" num={1} />
-      <Card className="card card-2-bg" num={2} />
+      <Card 
+        className="card card-0-bg" 
+        num={0}
+        projectTitle="Find LaCloche"
+        projectInfo={details.findLaCloche}
+      />
+      <Card
+        className="card card-1-bg"
+        num={1}
+        projectTitle="Find DIO"
+        projectInfo={details.finDIO}
+      />
+      <Card
+        className="card card-2-bg" 
+        num={2}
+        projectTitle="To Do"
+        projectInfo={details.toDo}
+      />
     </div>
   );
 }
