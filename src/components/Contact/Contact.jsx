a5f3597b7689ec3e815d0f7c03b4ee809c1eb965
Sheuh Xiong <sheuhxiong@gmail.com>
2021-04-13 20:43:04 -0400

    Adjusted margin spacing for Contact component

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -4,17 +4,23 @@ import "./Contact.css";
 function Contact() {
   return (
     <div className="contact">
-      <section className="section section-0">
+
+      <section className="section section-email">
+        <p><b>Email:</b></p>
         <p>sheuhxiong@gmail.com</p>
-        <p>or</p>
+      </section>
+      <p className="or-separator">Or</p>
+      <section className="section section-find-me-on">
         <p><b>Find me on:</b></p>
-        <a href="https://github.com/s92xiong" target="blank"><p>GitHub</p></a>
-        <a href="/" target="blank"><p>Resume</p></a>
+        <a href="https://github.com/s92xiong" target="blank">GitHub</a>
+        <br/>
+        <a href="/" target="blank">Resume</a>
       </section>
       
-      <section className="section-2">
+      <section className="section section-made-by-user">
         <p>Made by Sheuh Xiong © 2021</p>
       </section>
+
     </div>
   )
 }
