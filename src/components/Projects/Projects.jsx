c97e1261c6fc1da0b45e1e50667bcabd848a8cbf
Sheuh Xiong <sheuhxiong@gmail.com>
2021-08-29 21:29:47 -0400

    Updated bg images for cards, updated projects, removing Find Dio and To Do, adding The JoJo Club project

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -5,28 +5,20 @@ import "./Projects.css";
 function Projects() {
 
   const projects = {
+    jojoClub: {
+      title: "The JoJo Club",
+      description: "An exclusive clubhouse for JoJo members to write anonymous posts",
+      href: {
+        viewCode: "https://github.com/s92xiong/jojo-club",
+        livePreview: "https://jojo-club.herokuapp.com/"
+      }
+    },
     findLaCloche: {
       title: "Find LaCloche",
       description: "An online database for users to share images and reviews of campsites on the La Cloche Silhouette Trail",
       href: {
         viewCode: "https://github.com/s92xiong/find-la-cloche",
         livePreview: "https://findlacloche.com/"
-      }
-    },
-    finDIO: {
-      title: "Find DIO",
-      description: "A mini-game influenced by Where's Waldo: find the character DIO of JoJo's Bizarre Adventure",
-      href: {
-        viewCode: "https://github.com/s92xiong/find-DIO",
-        livePreview: "https://waldo-react-d81e7.firebaseapp.com/"
-      }
-    },
-    toDo: {
-      title: "To Do",
-      description: "A full-stack task management web-application inspired by Microsoft To Do",
-      href: {
-        viewCode: "https://github.com/s92xiong/tdl-react",
-        livePreview: "https://to-do-list-6d4f7.firebaseapp.com/"
       }
     },
     weatherApp: {
@@ -41,26 +33,19 @@ function Projects() {
 
   return (
     <div className="projects-container">
-      <Card 
+      <Card
         className="card card-0-bg" 
         num={0}
+        projectTitle={projects.jojoClub.title}
+        projectInfo={projects.jojoClub.description}
+        href={projects.jojoClub.href}
+      />
+      <Card 
+        className="card card-1-bg" 
+        num={1}
         projectTitle={projects.findLaCloche.title}
         projectInfo={projects.findLaCloche.description}
         href={projects.findLaCloche.href}
-      />
-      <Card
-        className="card card-1-bg"
-        num={1}
-        projectTitle={projects.finDIO.title}
-        projectInfo={projects.finDIO.description}
-        href={projects.finDIO.href}
-      />
-      <Card
-        className="card card-2-bg" 
-        num={2}
-        projectTitle={projects.toDo.title}
-        projectInfo={projects.toDo.description}
-        href={projects.toDo.href}
       />
       <Card
         className="card card-3-bg" 
