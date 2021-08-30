500b8b1c77a89768cbebe5eb0db4c63c22ceba83
Sheuh Xiong <sheuhxiong@gmail.com>
2021-08-29 21:53:18 -0400

    Removed resume from portfolio

--- README.md
+++ README.md
@@ -14,6 +14,8 @@ Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
 The page will reload if you make edits.\
 You will also see any lint errors in the console.
 
+Using node 13.12.0
+
 ### `npm test`
 
 Launches the test runner in the interactive watch mode.\
--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -13,8 +13,8 @@ function Contact() {
       <section className="section section-find-me-on">
         <p><b>Find me on:</b></p>
         <a href="https://github.com/s92xiong" target="blank">GitHub</a>
-        <br/>
-        <a href="https://firebasestorage.googleapis.com/v0/b/portfolio-3548a.appspot.com/o/sheuh_xiong_resume.pdf?alt=media&token=b8f6014f-6ba0-43e1-9670-c570510d8799" target="blank">Resume</a>
+        {/* <br/>
+        <a href="https://firebasestorage.googleapis.com/v0/b/portfolio-3548a.appspot.com/o/sheuh_xiong_resume.pdf?alt=media&token=b8f6014f-6ba0-43e1-9670-c570510d8799" target="blank">Resume</a> */}
       </section>
       
       <section className="section section-made-by-user">
