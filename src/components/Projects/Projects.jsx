3a9daa3a06f336605f58649d3a6ccbdd2d43fc34
Sheuh Xiong <sheuhxiong@gmail.com>
2021-10-18 20:07:27 -0400

    Added Blog Api to portfolio

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -5,6 +5,14 @@ import "./Projects.css";
 function Projects() {
 
   const projects = {
+    blogAPI: {
+      title: "Blog API",
+      description: "A simple blog writing website to share my interests and experiences",
+      href: {
+        viewCode: "https://github.com/s92xiong/blog-api",
+        livePreview: "https://blog-api-react-32131.firebaseapp.com/"
+      }
+    },
     jojoClub: {
       title: "The JoJo Club",
       description: "An exclusive clubhouse for JoJo members to write anonymous posts",
@@ -36,13 +44,20 @@ function Projects() {
       <Card
         className="card card-0-bg" 
         num={0}
+        projectTitle={projects.blogAPI.title}
+        projectInfo={projects.blogAPI.description}
+        href={projects.blogAPI.href}
+      />
+      <Card
+        className="card card-1-bg" 
+        num={1}
         projectTitle={projects.jojoClub.title}
         projectInfo={projects.jojoClub.description}
         href={projects.jojoClub.href}
       />
       <Card 
-        className="card card-1-bg" 
-        num={1}
+        className="card card-2-bg" 
+        num={2}
         projectTitle={projects.findLaCloche.title}
         projectInfo={projects.findLaCloche.description}
         href={projects.findLaCloche.href}
