87172c84e7be3be4e2644ca8f368beeca275aed9
Sheuh Xiong <sheuhxiong@gmail.com>
2022-03-19 17:21:59 -0400

    Updated Intro with workplace

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -4,8 +4,8 @@ import "./Intro.css";
 function Intro() {
   return (
     <div className="intro">
-      <p>I'm an aspiring web developer currently learning and building projects using the MERN stack.</p>
-      <p>My hobbies come and go. Recently, I've been playing Go (baduk), cooking, running, and learning more about web development to expand my technical skills. Check out my projects below!</p>
+      <p>I currently work on building <a href="https://www.i-sight.com/">i-Sight</a>, a case management platform for corporate investigations.</p>
+      <p>My hobbies come and go. Recently, I've been playing Go (baduk), cooking, running, and learning more about software development to expand my technical skills. Check out my projects below!</p>
     </div>
   );
 }
